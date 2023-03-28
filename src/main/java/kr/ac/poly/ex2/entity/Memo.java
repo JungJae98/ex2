cd263d37c9b5bd0ce5aef3143a5eb37f1e9411ca
aabds <wogus8880@gmail.com>
2023-03-28 16:30:07 +0900

    Query메\소드와 @Query(JPQL)

--- src/main/java/kr/ac/poly/ex2/entity/Memo.java
+++ src/main/java/kr/ac/poly/ex2/entity/Memo.java
@@ -1,2 +1,21 @@
-package kr.ac.poly.ex2.entity;public class Memo {
+package kr.ac.poly.ex2.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_memo")
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //IDENTITY가 auto increment
+    private int mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
 }
